37c74706bb3e619daaf44a0168c57bac907e85f7
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-01-31 10:50:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### EESDevOps-t2-iac1
---
+++ main.tf
@@ -0,0 +1,159 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.45.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+# Create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-eesdevops-dev-001"
+  location = "eastus2"
+
+  tags = {
+    environment = "DEV"
+    team        = "pos-graduacao"
+  }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dev-eastus2-001"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create sub network
+resource "azurerm_subnet" "snet" {
+  name                 = "snet-dev-eastus2-001"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Create public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-dev-eastus2-001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "lb-eesdevops-dev-001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  frontend_ip_configuration {
+    name                 = "publicIPAdress"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "lbap" {
+  resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.lb.id
+  name                = "BackendAddressPool"
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "nic${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipConfiguration"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+resource "azurerm_managed_disk" "disk" {
+  count                = 2
+  name                 = "datadisk_existing_${count.index}"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 1023
+}
+
+resource "azurerm_availability_set" "avset" {
+  name                         = "avset"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  count                 = 2
+  name                  = "vm${count.index}"
+  location              = azurerm_resource_group.rg.location
+  availability_set_id   = azurerm_availability_set.avset.id
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  # Optional data disks
+  storage_data_disk {
+    name              = "datadisk_new_${count.index}"
+    managed_disk_type = "Standard_LRS"
+    create_option     = "Empty"
+    lun               = 0
+    disk_size_gb      = "1023"
+  }
+
+  storage_data_disk {
+    name            = element(azurerm_managed_disk.disk.*.name, count.index)
+    managed_disk_id = element(azurerm_managed_disk.disk.*.id, count.index)
+    create_option   = "Attach"
+    lun             = 1
+    disk_size_gb    = element(azurerm_managed_disk.disk.*.disk_size_gb, count.index)
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
